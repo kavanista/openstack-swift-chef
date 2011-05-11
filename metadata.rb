3fa7a80c36d57ffa5adc18c5558b47b262d07933
James W. Brinkerhoff <jwb@paravolve.net>
2011-05-11 16:51:46 -0400

    added voxelized APL to all recipes and metadata

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "New Goliath, Inc."
-maintainer_email "judd@newgoliath.com"
+maintainer       "Voxel dot Net, Inc."
+maintainer_email "jwv@voxel.net"
 license          "Apache 2.0"
 description      "Installs/Configures openstack_swift"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
